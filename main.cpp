dc1f1ff2bff561b26ac4e386592dddba51491568
Davi-Motta <68289182+Davi-Motta@users.noreply.github.com>
2022-10-22 11:45:03 -0300

    Update main.cpp
    added
    - draw and win detection
    - pause so it doesn't close automatically when the game ends
    - prevented overriding already placed marks

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 char pos[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int taken[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 int victory = 0;
 int input = 0;
-int playerIndex = 5;
+int playerIndex = 1;
 char player = 'X';
-void board()
+bool draw = false;
+
+    void
+    board()
 {
-    cout << "\n \nPick a Number\n" << endl;
+    cout << "\n \nPick a Number\n"
+         << endl;
     cout << " " << pos[0] << " | " << pos[1] << " | " << pos[2] << endl;
     cout << "-----------" << endl;
     cout << " " << pos[3] << " | " << pos[4] << " | " << pos[5] << endl;
     cout << "-----------" << endl;
-    cout << " " << pos[6] << " | " << pos[7] << " | " << pos[8] << "\n" << endl;
+    cout << " " << pos[6] << " | " << pos[7] << " | " << pos[8] << "\n"
+         << endl;
 }
+
 int winCondition()
 {
     if (pos[0] == pos[1] && pos[1] == pos[2])
@@ -48,18 +57,42 @@ int winCondition()
     {
         return 1;
     }
-  return 0;
+        return 0;
 }
+
 int main()
 {
     board();
-    while (victory == 0){
+    while (victory == 0 && draw == false)
+    {
+
         player = (playerIndex % 2) ? 'X' : 'O';
         cin >> input;
-        pos[input - 1] = player;
+        if (taken[input - 1] == 0)
+        {
+            pos[input - 1] = player;
+            taken[input - 1] = 1;
+            playerIndex++;
+            cout << endl << playerIndex << endl;
+        }
+        if (playerIndex == 10){
+            draw = true;
+        }
         victory = winCondition();
         board();
-        playerIndex++;
     }
+
+    if (draw == true)
+    {
+        cout << "Draw! \n" << endl;
+        system("pause");
+    }
+    else
+    {
+    int winner = (player == 'X') ? 1 : 2;
+    cout << "Player " << winner << " wins! \n" << endl;
+    system("pause");
+    }
+    
     return 0;
 }
